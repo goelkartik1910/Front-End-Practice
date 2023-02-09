ffc6d5cda446908d698dbb9c709d6d1eec15d04d
Kartik Goel <kartikgoel.trainer@gmail.com>
2023-02-09 19:45:18 +0530

    js intro

---
+++ Javascript Basics/index.js
@@ -0,0 +1,11 @@
+var value = parseInt(prompt("Enter the age: "))
+console.log(typeof value, value);
+if(value >= 18){
+    console.log("Eligible")
+}
+else{
+    console.log("Not Eligible")
+}
+
+var result = confirm("Is user adult?")
+console.log(result);
